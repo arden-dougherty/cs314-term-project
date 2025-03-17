20b61561074838fbe3dabff639c4076274585146
Arden Dougherty <asd6@pdx.edu>
2025-03-17 12:27:48 -0700

    real time

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import Message from "../models/message.model.js"
+import {getReceiverSocketId, io} from "../lib/socket.js";
 
 export const getUserList = async (req, res) => {
     try {
@@ -46,7 +47,10 @@ export const sendMessage = async (req, res) => {
 
         await newMessage.save();
 
-        // realtime
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage)
     } catch (error) {
