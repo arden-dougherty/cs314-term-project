65f736a0b4a182dfdc21e35e2e0fb64a63007852
Arden Dougherty <asd6@pdx.edu>
2025-03-16 19:42:02 -0700

    messaging

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Message from "../models/message.model.js"
 
 export const getUserList = async (req, res) => {
     try {
@@ -11,3 +12,45 @@ export const getUserList = async (req, res) => {
         res.status(500).json({error: "Internal server error"});
     }
 };
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id:otherUserId} = req.params;
+        const thisUserId = req.user._id;
+        
+        const messages = await Message.find({
+            $or:[
+                {senderId:thisUserId, receiverId:otherUserId},
+                {senderId:otherUserId, receiverId:thisUserId}
+            ]
+        })
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller:", error.message);
+        res.status(500).json({error: 'Internal server error'});
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text
+        });
+
+        await newMessage.save();
+
+        // realtime
+
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("Error in sendMessage controller:", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js"
-import {getUserList} from "../controllers/message.controller.js";
+import {getUserList, getMessages, sendMessage} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUserList)
+
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
